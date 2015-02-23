ba148c61e14223da614c9d054c869750bc1bd0e0
Grant Toeppen <grant@adroll.com>
2015-02-23 23:28:44 +0000

    Catch the KeyError that occasionally occurs on Facebook responses

--- facebookads/exceptions.py
+++ facebookads/exceptions.py
@@ -72,11 +72,11 @@ class FacebookRequestError(FacebookError):
                 self._api_error_type = self._error['type']
             if 'error_data' in self._error:
                 try:
-                    self._api_blame_field_specs = \
-                        self._error['error_data']['blame_field_specs']
-                except TypeError:
+                    self._api_blame_field_specs = self._error['error_data']['blame_field_specs']
+                except TypeError, KeyError:
                     # The response from Facebook is occasionally malformed and
                     # raises `TypeError: list indices must be integers, not str`
+                    # or 'KeyError: 'blame_field_specs'
                     # Missing `blame_field_specs` isn't a big enough problem
                     # to crash, so we'll just continue
                     pass
